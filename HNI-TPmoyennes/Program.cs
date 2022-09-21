395bb99f392db77307fc29f4a0fab95f6b844cc2
Marwa Ters <marwaters94@gmail.com>
2022-09-21 15:59:10 +0200

    methode ajouterMatiere ajouté

--- HNI-TPmoyennes/Program.cs
+++ HNI-TPmoyennes/Program.cs
@@ -20,45 +20,45 @@ namespace TPMoyennes
               sixiemeA.ajouterEleve("Sacha", "TOUILLE");
               sixiemeA.ajouterEleve("Cesar", "TICHO");
               sixiemeA.ajouterEleve("Guy", "DON");
-            /* 
+             
              // Ajout de matières étudiées par la classe
              sixiemeA.ajouterMatiere("Francais");
              sixiemeA.ajouterMatiere("Anglais");
              sixiemeA.ajouterMatiere("Physique/Chimie");
              sixiemeA.ajouterMatiere("Histoire");
              Random random = new Random();
-             // Ajout de 5 notes à chaque élève et dans chaque matière
-
-             for (int ieleve = 0; ieleve < sixiemeA.eleves.Count(); ieleve++)
-             {
-                 for (int matiere = 0; matiere < sixiemeA.matieres.Count(); matiere++)
-                 {
-                     for (int i = 0; i < 5; i++)
-                     {
-                         if (sixiemeA.eleves[ieleve] is not null)
+            // Ajout de 5 notes à chaque élève et dans chaque matière
+            /*
+                         for (int ieleve = 0; ieleve < sixiemeA.eleves.Count(); ieleve++)
                          {
-                             sixiemeA.eleves[ieleve].ajouterNote(new Note(matiere, (float)((6.5 + random.NextDouble() * 34)) / 2.0f));
+                             for (int matiere = 0; matiere < sixiemeA.matieres.Count(); matiere++)
+                             {
+                                 for (int i = 0; i < 5; i++)
+                                 {
+                                     if (sixiemeA.eleves[ieleve] is not null)
+                                     {
+                                         sixiemeA.eleves[ieleve].ajouterNote(new Note(matiere, (float)((6.5 + random.NextDouble() * 34)) / 2.0f));
 
+                                     }
+                                     // Note minimale = 3
+                                 }
+                             }
                          }
-                         // Note minimale = 3
+                         Console.WriteLine("");
+                         Eleve eleve = sixiemeA.eleves[6];
+                         // Afficher la moyenne d'un élève dans une matière
+                         Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
+                         eleve.Moyenne(1) + "\n");
+                         // Afficher la moyenne générale du même élève
+                         Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne Generale : " + eleve.Moyenne() + "\n");
+                         // Afficher la moyenne de la classe dans une matière
+                         Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
+                         sixiemeA.Moyenne(1) + "\n");
+                         // Afficher la moyenne générale de la classe
+                         Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne Generale : " + sixiemeA.Moyenne() + "\n");
+                         Console.Read();
                      }
-                 }
-             }
-             Console.WriteLine("");
-             Eleve eleve = sixiemeA.eleves[6];
-             // Afficher la moyenne d'un élève dans une matière
-             Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
-             eleve.Moyenne(1) + "\n");
-             // Afficher la moyenne générale du même élève
-             Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne Generale : " + eleve.Moyenne() + "\n");
-             // Afficher la moyenne de la classe dans une matière
-             Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
-             sixiemeA.Moyenne(1) + "\n");
-             // Afficher la moyenne générale de la classe
-             Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne Generale : " + sixiemeA.Moyenne() + "\n");
-             Console.Read();
-         }
-   */
+               */
         }
         class Note : Eleve
         {
@@ -85,8 +85,9 @@ namespace TPMoyennes
             }
             public void ajouterEleve(string prenom, string nom)
             {
-                for (int i = 0; i < this.eleves.Length; i++)
+                for (int i = 0; i < eleves.Length; i++)
                 {
+                    //verification si tab eleve est null
                     if (eleves[i] is null)
                     {
                         eleves[i] = new Eleve(prenom, nom);
@@ -94,6 +95,18 @@ namespace TPMoyennes
                     }
                 }
               //  Console.WriteLine($"{prenom} {nom}");
+            }
+            public void ajouterMatiere(string matiere)
+            {
+                for (int i = 0; i < matieres.Length; i++)
+                {
+                    if (matieres[i] is null)
+                    {
+                        matieres[i] = matiere;
+                        break;
+                    }
+                }
+               //Console.WriteLine(matiere);
             }
 
         }
