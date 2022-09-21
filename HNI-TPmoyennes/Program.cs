d587f8f5245976dbd17c804f1949b05defa29669
Marwa Ters <marwaters94@gmail.com>
2022-09-21 15:53:28 +0200

    methode ajouterEleve ajouté

--- HNI-TPmoyennes/Program.cs
+++ HNI-TPmoyennes/Program.cs
@@ -11,7 +11,7 @@ namespace TPMoyennes
             // Création d'une classe
             Classe sixiemeA = new Classe("6eme A");
             // Ajout des élèves à la classe
-            /*  sixiemeA.ajouterEleve("Jean", "RAGE");
+             sixiemeA.ajouterEleve("Jean", "RAGE");
               sixiemeA.ajouterEleve("Paul", "HAAR");
               sixiemeA.ajouterEleve("Sibylle", "BOQUET");
               sixiemeA.ajouterEleve("Annie", "CROCHE");
@@ -20,48 +20,45 @@ namespace TPMoyennes
               sixiemeA.ajouterEleve("Sacha", "TOUILLE");
               sixiemeA.ajouterEleve("Cesar", "TICHO");
               sixiemeA.ajouterEleve("Guy", "DON");
-              //afficheEleves(sixiemeA);
-              // Ajout de matières étudiées par la classe
-              sixiemeA.ajouterMatiere("Francais");
-              sixiemeA.ajouterMatiere("Anglais");
-              sixiemeA.ajouterMatiere("Physique/Chimie");
-              sixiemeA.ajouterMatiere("Histoire");
-              Random random = new Random();
-              // Ajout de 5 notes à chaque élève et dans chaque matière
-              //Console.WriteLine($"sixiemeA.eleves.Count(): {sixiemeA.eleves.Count()}");
-              //Console.WriteLine($"sixiemeA.eleves.Length: {sixiemeA.eleves.Length}");
-              for (int ieleve = 0; ieleve < sixiemeA.eleves.Count(); ieleve++)
-              {
-                  for (int matiere = 0; matiere < sixiemeA.matieres.Count(); matiere++)
-                  {
-                      for (int i = 0; i < 5; i++)
-                      {
-                          //afficheEleves(sixiemeA);
-                          if (sixiemeA.eleves[ieleve] is not null)
-                          {
-                              sixiemeA.eleves[ieleve].ajouterNote(new Note(matiere, (float)((6.5 + random.NextDouble() * 34)) / 2.0f));
-                              //Note uneNote = new Note(matiere, (float)((6.5 + random.NextDouble() * 34)) / 2.0f);
-                              //sixiemeA.eleves[ieleve].ajouterNote(uneNote);
-                          }
-                          // Note minimale = 3
-                      }
-                  }
-              }
-              Console.WriteLine("");
-              Eleve eleve = sixiemeA.eleves[6];
-              // Afficher la moyenne d'un élève dans une matière
-              Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
-              eleve.Moyenne(1) + "\n");
-              // Afficher la moyenne générale du même élève
-              Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne Generale : " + eleve.Moyenne() + "\n");
-              // Afficher la moyenne de la classe dans une matière
-              Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
-              sixiemeA.Moyenne(1) + "\n");
-              // Afficher la moyenne générale de la classe
-              Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne Generale : " + sixiemeA.Moyenne() + "\n");
-              Console.Read();
-          }
-    */
+            /* 
+             // Ajout de matières étudiées par la classe
+             sixiemeA.ajouterMatiere("Francais");
+             sixiemeA.ajouterMatiere("Anglais");
+             sixiemeA.ajouterMatiere("Physique/Chimie");
+             sixiemeA.ajouterMatiere("Histoire");
+             Random random = new Random();
+             // Ajout de 5 notes à chaque élève et dans chaque matière
+
+             for (int ieleve = 0; ieleve < sixiemeA.eleves.Count(); ieleve++)
+             {
+                 for (int matiere = 0; matiere < sixiemeA.matieres.Count(); matiere++)
+                 {
+                     for (int i = 0; i < 5; i++)
+                     {
+                         if (sixiemeA.eleves[ieleve] is not null)
+                         {
+                             sixiemeA.eleves[ieleve].ajouterNote(new Note(matiere, (float)((6.5 + random.NextDouble() * 34)) / 2.0f));
+
+                         }
+                         // Note minimale = 3
+                     }
+                 }
+             }
+             Console.WriteLine("");
+             Eleve eleve = sixiemeA.eleves[6];
+             // Afficher la moyenne d'un élève dans une matière
+             Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
+             eleve.Moyenne(1) + "\n");
+             // Afficher la moyenne générale du même élève
+             Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne Generale : " + eleve.Moyenne() + "\n");
+             // Afficher la moyenne de la classe dans une matière
+             Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
+             sixiemeA.Moyenne(1) + "\n");
+             // Afficher la moyenne générale de la classe
+             Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne Generale : " + sixiemeA.Moyenne() + "\n");
+             Console.Read();
+         }
+   */
         }
         class Note : Eleve
         {
@@ -86,6 +83,18 @@ namespace TPMoyennes
                 this.eleves = new Eleve[30];
                 this.matieres = new string[10];
             }
+            public void ajouterEleve(string prenom, string nom)
+            {
+                for (int i = 0; i < this.eleves.Length; i++)
+                {
+                    if (eleves[i] is null)
+                    {
+                        eleves[i] = new Eleve(prenom, nom);
+                        break;
+                    }
+                }
+              //  Console.WriteLine($"{prenom} {nom}");
+            }
 
         }
 
@@ -94,7 +103,8 @@ namespace TPMoyennes
             internal Note[] notes;
             public string prenom;
             public string nom;
-            public Eleve(string nom = "", string prenom = "")
+            //chaine caractere vide pour nom et prenom
+            public Eleve(string prenom = "", string nom = "")
             {
                 this.nom = nom;
                 this.prenom = prenom;
